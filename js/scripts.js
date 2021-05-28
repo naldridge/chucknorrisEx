53f129d7c27f9f05ef152f9079587e78b1fea922
Nick Aldridge <naldridge1360@gmail.com>
2021-05-28 11:13:04 -0500

    maintained currentcategory and updated get quote button

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,5 @@
 'use strict';
-const getQuote = document.querySelector('#getQuote');
-const lookAtCategory = document.querySelector('#categoryList');
+
 //XHR Example
 /* const request = new XMLHttpRequest();
 request.onreadystatechange = function () {
@@ -11,56 +10,87 @@ request.onreadystatechange = function () {
 }
 request.open('GET', 'https://api.chucknorris.io/jokes/random?category=dev');
 request.send(); */
-
-function fetchTheQuote(category) {
-    fetch(`https://api.chucknorris.io/jokes/random?category=${category}`
-    ).then(function (response) {
-        console.log("Fetch Response is: ", response);
-        return response.json();
-    }).then(function (data) {
-        updateQuote(data);
-    });
-}
-
-function fetchTheCategories() {
-    fetch('https://api.chucknorris.io/jokes/categories').then(function (response) {
-    return response.json();
-    }).then(function (data) {
+document.addEventListener('DOMContentLoaded', function () {
+    const getQuote = document.querySelector('#getQuote');
+    const lookAtCategory = document.querySelector('#categoryList');
+    const chuckQuote = document.querySelector('#chuckQuote');
+    const defaultCategory = 'dev';
+    let currentCategory = defaultCategory;
     
-        updateCategories(data);
+
+
+    function fetchTheQuote(category) {
+        fetch(`https://api.chucknorris.io/jokes/random?category=${category}`
+        ).then(function (response) {
+            console.log("Fetch Response is: ", response);
+            return response.json();
+        }).then(function (data) {
+            updateQuote(data);
+        });
+    }
+
+    function fetchTheCategories() {
+        fetch('https://api.chucknorris.io/jokes/categories').then(function (response) {
+            return response.json();
+        }).then(function (data) {
+
+            updateCategories(data);
+        });
+    }
+
+    function updateQuote(data) {
+        const theQuote = data.value;
+        console.log(theQuote);
+        chuckQuote.innerText = theQuote;
+
+    };
+
+    function updateCategories(categoryData) {
+        const selectElement = document.createElement('select');
+        const filteredList = categoryData.filter(function(category) {
+            if (category !== 'explicit' && category !== 'political' && category !== 'sport') {
+                return category;
+            }
+        })
+
+        
+    
+
+        filteredList.forEach(function (category) {
+            const categoryOptionElement = document.createElement('option');
+            categoryOptionElement.value = category;
+            categoryOptionElement.text = category;
+            if (category === currentCategory) {
+                categoryOptionElement.setAttribute('selected', true);
+            }
+            selectElement.appendChild(categoryOptionElement);
+        });
+
+        lookAtCategory.appendChild(selectElement);
+
+        selectElement.addEventListener('change', function (event) {
+            console.log('The Event is :', event.target.value);
+            const newcategoryName = event.target.value;
+            currentCategory = newcategoryName;
+            fetchTheQuote(currentCategory);
+        });
+    };
+
+    getQuote.addEventListener('click', function () {
+        fetchTheQuote(currentCategory);
     });
 
-}
-
-const chuckQuote = document.querySelector('#chuckQuote');
-
-function updateQuote(data) {
-    const theQuote = data.value;
-    console.log(theQuote);
-    chuckQuote.innerText = theQuote;
-
-}
-
-function updateCategories(categoryData) {
-    const selectElement = document.createElement('select');
-    categoryData.forEach(function(category) {
-        const categoryOptionElement = document.createElement('option');
-        categoryOptionElement.value = category;
-        categoryOptionElement.text = category;
-        selectElement.appendChild(categoryOptionElement);
-    });
-    lookAtCategory.appendChild(selectElement);
-
-    selectElement.addEventListener('change', function (event) {
-        console.log('The Event is :', event.target.value);
-        const categoryName = event.target.value;
-        fetchTheQuote(categoryName);
-    })
-}
-
-getQuote.addEventListener('click', function () {
-    fetchTheQuote();
+    fetchTheQuote(currentCategory);
+    fetchTheCategories();
 });
 
-fetchTheQuote('animal');
-fetchTheCategories();
+
+
+
+
+
+
+
+
+
+
